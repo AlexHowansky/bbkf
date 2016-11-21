d063feb763292368efde196c404d536684a08ef1
Alex Howansky <alex.howansky@gmail.com>
2016-11-21 18:53:25 +0000

    Added BF to BBKF image generation.

--- src/BooBooKittyFuck.php
+++ src/BooBooKittyFuck.php
@@ -86,6 +86,10 @@ class BooBooKittyFuck
      */
     public function getImage()
     {
+        if ($this->image instanceof \imagick === false) {
+            throw new \RuntimeException('Image must first be set with setImage() or setSource().');
+        }
+        return $this->image;
     }
 
     /**
@@ -105,6 +109,9 @@ class BooBooKittyFuck
      */
     public function getSource()
     {
+        if ($this->source === null) {
+            throw new \RuntimeException('Source must first be set with setImage() or setSource().');
+        }
         return $this->source;
     }
 
@@ -131,6 +138,9 @@ class BooBooKittyFuck
     /**
      * Set the image for this program.
      *
+     * This will open the image file and analyze it against the
+     * list of instruction images, converting to BF source.
+     *
      * @param string $file The source file containing the image.
      *
      * @return BooBooKittyFuck Allow method chaining.
@@ -147,7 +157,7 @@ class BooBooKittyFuck
             $this->image->getImageWidth() % $this->getTileSize() !== 0 ||
             $this->image->getImageHeight() % $this->getTileSize() !== 0
         ) {
-            throw new \RuntimeException('Image size is not a multiple of tile size.');
+            throw new \RuntimeException('Image dimensions must be a multiple of tile size.');
         }
 
         $this->source = '';
@@ -185,6 +195,9 @@ class BooBooKittyFuck
      */
     public function setImagePath($path)
     {
+        if (is_dir(realpath($path)) === false) {
+            throw new \RuntimeException($dir . ' is not a directory.');
+        }
         $this->imagePath = rtrim($path, '/') . '/';
         foreach (self::INSTRUCTIONS as $instruction => $name) {
             $file = $this->imagePath . $name . '.jpg';
@@ -199,13 +212,47 @@ class BooBooKittyFuck
     /**
      * Set the Brainfuck source for this program.
      *
-     * @param string $bf The Brainfuck source for this program
+     * @param string $bf The Brainfuck source for this program.
      *
      * @return BooBooKittyFuck Allow method chaining.
      */
     public function setSource($bf)
     {
+
+        // Try to determine if the parameter is a file name or source code.
+        $regex = '/[^\\' . implode('\\', array_keys(self::INSTRUCTIONS)) . ']/';
+        if (preg_match($regex, $bf) === 1 && file_exists($bf) === true) {
+            $bf = file_get_contents($bf);
+        }
+
+        // Strip non-BF characters.
+        $this->source = preg_replace($regex, '', $bf);
+
+        $num = 0;
+        $size = strlen($this->source);
+        $xsize = ceil(sqrt($size));
+        $ysize = ceil($size / $xsize);
+        $montage = new \imagick();
+
+        for ($y = 0; $y < $ysize; $y++) {
+            for ($x = 0; $x < $xsize; $x++) {
+                if ($num < $size) {
+                    $montage->addImage($this->cats[$this->source[$num++]]);
+                }
+            }
+        }
+
+        $this->image = $montage->montageImage(
+            new \imagickDraw(),
+            $xsize . 'x' . $ysize . '+0+0',
+            $this->tileSize . 'x' . $this->tileSize . '+0+0',
+            \imagick::MONTAGEMODE_UNFRAME,
+            '0x0+0+0'
+        );
+        $this->image->setImageFormat('jpg');
+
         return $this;
+
     }
 
     /**
